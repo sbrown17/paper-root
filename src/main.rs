83759cab24ea2aea242c2e0e26ddef84329f8ec2
unknown <stubrown357@gmail.com>
2025-08-10 11:44:26 -0500

    add packages needed for choosing wallpapers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use rand::prelude::*;
+use wallpaper;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting paper-root...");
+    // get season and time of day (eg. winter, midday)
+    // grab vec of image filenames within that path
+    // random number from 1..veclength
+    // set wallpaper
+    // wallpaper::set_from_path
 }
