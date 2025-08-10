b9e89074a38abe9a30c72996db146cb5b3b8b10b
unknown <stubrown357@gmail.com>
2025-08-10 11:57:07 -0500

    use chrono to get readable datetimes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use chrono::prelude::*;
 use rand::prelude::*;
 use wallpaper;
 
 fn main() {
     println!("Starting paper-root...");
+    let date_time_string = Utc::now().to_string();
+    println!("{}", date_time_string);
     // get season and time of day (eg. winter, midday)
     // grab vec of image filenames within that path
     // random number from 1..veclength
